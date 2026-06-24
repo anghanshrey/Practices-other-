3554290317bcbbe6d751834053212393d739333d
Shrey Anghan <shreyanghan1@gmail.com>
2026-06-24 16:40:35 +0530

    24-6-2026

---
+++ 24-6-2026.py
@@ -0,0 +1,93 @@
+"""
+    print("Hello, my name is Shrey")
+"""
+
+"""
+    a = 5
+    b = 10
+    sum = a + b
+    print("the sum is :", sum)
+"""
+
+"""
+    name = input("What is your name")
+
+    print(name)
+    
+"""
+
+"""
+    a = int(input("Enter First number :"))
+    b = int(input("Enter Second number :"))
+
+    print("Addition:", a+b)
+    print("subtraction :", a-b)
+    print("Multiplication :", a*b)
+    print("Division :", a/b)
+"""
+
+"""
+    print("formatting using sep and end")
+    print("Apple","Banana","cherry" , sep="|" , end = ">--------------End of list\n")
+"""
+
+"""
+    name = input("Enter Your name:")
+    age = int(input("Enter your age:"))
+    Hobby = input("Enter your favorite hobby:")
+
+    print(f"Hello ,{name} at {age} Enjoying {Hobby}Sounds fun!!!")
+"""
+
+"""
+# Declare Variable of diffent data type and their types
+
+a = 10
+b = 3.14
+c = "python"
+d = True
+
+print(a,"type",type(a))
+print(b,"type",type(b))
+print(c,"type",type(c))
+print(d,"type",type(d))
+"""
+
+
+"""
+# pytohn program
+
+Subject1 = input("Enter 1 Subject name :")
+marks1 = int(input("Enter your marks :"))
+
+Subject2 = input("Enter 2 Subject name :")
+marks2 = int(input("Enter your marks :"))
+
+Subject3 = input("Enter 3 Subject name :")
+marks3 = int(input("Enter your marks :"))
+
+# Decide total & average:
+
+total = marks1+marks2+marks3
+average = total/3
+
+# Decide Grade:
+
+if average >= 90 :
+    grade = "A"
+elif average >= 75 :
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("\n=========Result===============")
+
+print(f"{Subject1} : {marks1}")
+print(f"{Subject2} : {marks2}")
+print(f"{Subject3} : {marks3}")
+print(f"Total Marks :{total} and Average : {average}")
+"""
