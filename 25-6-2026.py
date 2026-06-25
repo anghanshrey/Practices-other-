bfa498dade2e0b5c2d752a41ca87fa71933fa3fb
Shrey Anghan <shreyanghan1@gmail.com>
2026-06-25 16:19:13 +0530

    25-6-2026

---
+++ 25-6-2026.py
@@ -0,0 +1,120 @@
+"""
+# Arithmetic operator:
+
+a = 10
+b = 20
+
+print("Addition", a+b)
+print("subtraction", a-b)
+print("multiplication", a*b)
+print("Division", a/b)
+print("Modulus", a%b)
+print("Exponentiation", a**b)
+print("Floor Division", a//b)
+
+"""
+
+"""
+
+#Assignment operator:
+
+x = 5
+
+y = 3
+
+x += y
+
+print(x)
+print(y)
+
+x -= y
+
+print(x)
+print(y)
+
+x *= y
+
+print(x)
+print(y)
+
+x /= y
+
+print(x)
+print(y)
+
+x %= y
+
+print(x)
+print(y)
+
+x **= y
+
+print(x)
+print(y)
+
+x //= y
+
+print(x)
+print(y)
+
+"""
+
+"""
+
+#comparision operator:
+
+x = 10
+y = 10
+
+print ( x == y )
+print ( x != y )
+print ( x <= y )
+print ( x >= y )
+print ( x > y )
+print ( x < y )
+
+"""
+
+"""
+
+# logical operator:
+
+x = True
+y = False
+z = False
+
+print( x and y )
+print( z and y )
+print( x or y )
+print( z or y )
+print( not x )
+print( not y )
+
+"""
+
+"""
+# type conversion
+
+num_str = "123"
+print(num_str)
+
+num_int = int(num_str)
+print(num_int)
+
+"""
+
+"""
+
+# multiple assigment:
+
+x,y,z = 10,20,30
+
+print(x,y,z)
+
+a = 20
+b = 21
+
+print(id(a))
+print(id(b))
+
+"""
