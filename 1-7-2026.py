6f130a79485c1f8347354243b314093e882d4f1c
Shrey Anghan <shreyanghan1@gmail.com>
2026-07-01 23:25:46 +0530

    1-7-2026

---
+++ 1-7-2026.py
@@ -0,0 +1,71 @@
+#Q-1
+"""
+for i in range(1,20):
+    print(i)
+
+for i in range(1,20):
+    if i%4==0:
+        print(i)
+"""
+
+#Q-2
+"""
+for i in range(1,10):
+    print(i)
+
+for i in range(1,10):
+    if i == 7:
+        break
+    print(i)
+"""
+
+#Q-3
+"""
+name = "python"
+
+for i in name:
+    if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+        print(f"{i} is vowels.")
+    else:
+        print(f"{i} is consonants.")
+"""
+
+#Q-4
+"""
+N = int(input("Enter a Number :"))
+
+for i in range(1,11):
+    print(f"{N}X{i}={N*i}")
+"""
+
+#Q-5
+"""
+for i in range(1,6):
+    for j in range(1,i + 1):
+        print(j,end=" ")
+    print("")
+"""
+
+#Q-6
+"""
+for i in range(5,0,-1):
+    for j in range(5,i-1,-1):
+        print(j,end=" ")
+    print("")
+"""
+
+#Q-7
+"""
+for i in range(5,0,-1):
+    for j in range(i,6):
+        print(j,end=" ")
+    print(" ")
+"""
+
+#Q-8
+"""
+for i in range(1,6):
+    for j in range(i,6):
+        print(j,end=" ")
+    print(" ")
+"""
---
+++ 30-6-2026/30-6-2026(Types_of_control_statement).py
@@ -0,0 +1,52 @@
+# Q-1
+"""
+number = int(input("Enter your number:"))
+
+if number % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
+"""
+
+# Q-2
+"""
+age = int(input("Enter your age:"))
+
+if age >= 0 and age <= 12:
+    print("Child")
+elif age >= 13 and age <=19:
+    print("Teenager")
+elif age >= 20 and age <=59:
+    print("Adult")
+elif age >= 60:
+    print("Senior")
+else:
+    print("Invaild age")
+"""
+
+# Q-3
+"""
+num1= int(input("Enter First Number:"))
+num2= int(input("Enter Second Number:"))
+num3= int(input("Enter Third Number:"))
+
+if num1 > num2 and num1 > num3:
+    print(f"{num1} is largest number.")
+elif num2 > num1 and num2 > num3:
+    print(f"{num2} is largest number.")
+else:
+    print(f"{num3} is largest number.")
+"""
+
+# Q-4
+"""
+num = float(input("Enter a number:"))
+
+if num == 0:
+    print("Neutral number (Zero)")
+elif num > 0:
+    print("Positive number")
+else:
+    print("Negative number")
+"""
+
