7ecbda84e574ce46483379318ea40e20c820d152
Shrey Anghan <shreyanghan1@gmail.com>
2026-08-12 22:37:32 +0530

    update

--- 9/9.4.py
+++ 9/9.4.py
@@ -160,5 +160,137 @@ print("="*40)
 print("Q-7")
 print("="*40)
 
+print("="*40)
+print("Q-8")
+print("="*40)
 
+"""
+class Vehicle:
 
+    def start(self):
+        print("Start Vehicle....")
+
+class bike(Vehicle):
+
+    def start(self):
+        print("Start Bike......")
+
+class Car(Vehicle):
+
+    def start(self):
+        print("Start Car......")
+
+vehicles = [bike(), Car()]
+
+for veh in vehicles:
+    veh.start()
+"""
+
+print("="*40)
+print("Q-9")
+print("="*40)
+
+"""
+class Printer:
+    def print_data(self, arg1=None, arg2= None):
+        if arg1 is not None and arg2 is not None:
+            print(f"Both: {arg1} and {arg2}")
+        elif arg1 is not None:
+            if isinstance(arg1, str):
+                print(f"String: {arg1}")
+            elif isinstance(arg1, int):
+                print(f"Integer: {arg1}")
+        else:
+            print("No arguments provided")
+
+p = Printer()
+p.print_data("Hello")
+p.print_data(45)
+p.print_data("Age", 21)
+"""
+
+print("="*40)
+print("Q-10")
+print("="*40)
+
+"""
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class Student(Person):
+    def __init__(self, name, student_id):
+        super().__init__(name)
+        self.student_id = student_id
+
+result = issubclass(Student, Person)
+print(f"Is Student a subclass of Person := {result}")
+"""
+
+print("="*40)
+print("Q-11")
+print("="*40)
+
+"""
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+        print(f"Employee Initialized: {self.name}, Salary Initialized: {self.salary}")
+
+class Manager(Employee):
+
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = department
+        print(f"Manager initialized for department: {self.department}")
+
+mgr = Manager("Alice", 90000 , "IT")
+"""
+
+print("="*40)
+print("Q-12")
+print("="*40)
+"""
+class Grandparent:
+
+    def display(self):
+        print("Grandparent display.")
+
+class Parent(Grandparent):
+
+    def display(self):
+        print("Parent display.")
+
+class Child(Parent):
+
+    def display(self):
+        print("Child display.")
+
+print(f"Is subclass Relationship : {issubclass(Child, Parent)}")
+print(f"Is subclass Relationship : {issubclass(Child, Grandparent)}")
+print(f"Is subclass Relationship : {issubclass(Parent, Grandparent)}")
+"""
+
+print("="*40)
+print("Q-13")
+print("="*40)
+
+"""
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        print(f"User profile created for: {self.username}")
+
+class Admin(User):
+    def __init__(self, username, email, access_level):
+        # Call the parent User constructor
+        super().__init__(username, email)
+        self.access_level = access_level
+        print(f"Admin level set to: {self.access_level}")
+
+# Testing the code
+admin_user = Admin("john_doe", "john@example.com", "Full Access")
+"""
