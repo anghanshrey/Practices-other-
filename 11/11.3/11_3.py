7cf61d6597567380264c883118e6c9c29318538b
Shrey Anghan <shreyanghan1@gmail.com>
2026-08-19 18:18:01 +0530

    update

---
+++ 11/11.3/11_3.py
@@ -0,0 +1,34 @@
+print("="*40)
+print("Q-1")
+print("="*40)
+
+"""
+number = int(input("Enter a Number : "))
+
+if number < 0:
+    raise ValueError("User enters a negative number.")
+
+print("Number : ", number)
+"""
+
+print("="*40)
+print("Q-2")
+print("="*40)
+"""
+def check_even(number):
+
+    if number == int:
+        raise TypeError("The input is not an integer.")
+    if number%2 == 1:
+        raise ValueError("The number is odd.")
+    print(f"Even Number is : {number}")
+
+number = int(input("Enter a Number : "))
+
+check_even(number)
+"""
+
+print("="*40)
+print("Q-3")
+print("="*40)
+
