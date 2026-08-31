b9d6792c0b82aa6967d2deafeec093973f6db1a9
Shrey Anghan <shreyanghan1@gmail.com>
2026-08-31 15:51:36 +0530

    update

--- 13/13_3.py
+++ 13/13_3.py
@@ -3,7 +3,7 @@ print("Q-1")
 print("="*40)
 
 import uuid
-
+"""
 random_uuid = uuid.uuid4()
 
 print(f"Random UUID: {random_uuid}")
@@ -12,7 +12,29 @@ name = "Example.com"
 
 named_uuid = uuid.uuid3(uuid.NAMESPACE_DNS, name)
 print(f"named_uuid: {named_uuid}")
+"""
 
 print("="*40)
 print("Q-2")
 print("="*40)
+"""
+student_ids = ["S101", "S102", "S103", "S104", "S105"]
+student_dict = {}
+
+for s_id in student_ids:
+    student_dict[s_id] = uuid.uuid4()
+
+print("Student Database:", student_dict)
+"""
+
+print("="*40)
+print("Q-3")
+print("="*40)
+
+uuid1 = uuid.uuid4()
+uuid2 = uuid.uuid4()
+
+if uuid1 == uuid2:
+    print("UUIDs are equal.")
+else:
+    print("UUIDs are not equal.")
